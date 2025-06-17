a0c2568d1178bd2fcc611010c0ad36c1e7bc1158
kovacsgergely22 <113249160+kovacsgergely22@users.noreply.github.com>
2025-06-17 21:10:36 +0200

    add backen

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
 const express = require('express');
-const PORT = 5000;
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 8000;
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API' });
+});
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Error handling middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
