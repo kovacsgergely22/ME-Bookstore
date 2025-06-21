530467a1e768691edd030c8b9447cbd4a450a42b
kovacsgergely22 <113249160+kovacsgergely22@users.noreply.github.com>
2025-06-21 20:44:05 +0200

    update - bookController with controlling functions

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -7,16 +7,49 @@ const Book = require('../models/BookModel');
 // @route   GET /api/books
 // @access  Private
 const getBooks = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
 
-    res.status(200).json({ message: 'getBooks' });
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    const books = await Book.find({ user: req.user.id });
+
+    res.status(200).json({ books });
 });
 
 // @desc    Create new books
 // @route   POST /api/books
 // @access  Private
 const createBook = asyncHandler(async (req, res) => {
+    const {category, author, title, publisher, year, isFavorite} = req.body;
 
-    res.status(200).json({ message: 'createBooks' });
+    if (!category || !author || !title || !publisher || !year) {
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
+
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    const book = await Book.create({
+        user: req.user.id,
+        category,
+        author,
+        title,
+        publisher,
+        year,
+        isFavorite: isFavorite || false
+    });
+
+    res.status(201).json({ book });
 });
 
 module.exports = {
