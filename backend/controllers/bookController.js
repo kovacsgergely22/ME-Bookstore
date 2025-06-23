8d3ac5823a104b4a027cdad1b1b2e3bba4adbf81
kovacsgergely22 <113249160+kovacsgergely22@users.noreply.github.com>
2025-06-23 14:39:34 +0200

    Add - CRUD operations to the backend

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -20,11 +20,38 @@ const getBooks = asyncHandler(async (req, res) => {
     res.status(200).json({ books });
 });
 
+// @desc    Get user book
+// @route   GET /api/books/:id
+// @access  Private
+const getBook = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+        res.status(404);
+        throw new Error('Book not found');
+    }
+
+    if (book.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error('User not authorized to view this book');
+    }
+
+    res.status(200).json({ book });
+});
+
 // @desc    Create new books
 // @route   POST /api/books
 // @access  Private
 const createBook = asyncHandler(async (req, res) => {
-    const {category, author, title, publisher, year, isFavorite} = req.body;
+    const { category, author, title, publisher, year, isFavorite } = req.body;
 
     if (!category || !author || !title || !publisher || !year) {
         res.status(400);
@@ -52,7 +79,72 @@ const createBook = asyncHandler(async (req, res) => {
     res.status(201).json({ book });
 });
 
+// @desc    Delete book
+// @route   DELETE /api/books/:id
+// @access  Private
+const deleteBook = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+        res.status(404);
+        throw new Error('Book not found');
+    }
+
+    if (book.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error('User not authorized to view this book');
+    }
+
+    await book.deleteOne();
+
+    res.status(200).json({ success: true });
+});
+
+// @desc    Update book
+// @route   PUT /api/books/:id
+// @access  Private
+const updateBook = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+        res.status(404);
+        throw new Error('Book not found');
+    }
+
+    if (book.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error('User not authorized to view this book');
+    }
+
+    const updateBook = await Book.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.status(200).json(updateBook);
+});
+
 module.exports = {
     getBooks,
-    createBook
+    getBook,
+    createBook,
+    deleteBook,
+    updateBook,
 };
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getBooks, createBook } = require('../controllers/bookController');
+const { getBooks, getBook, createBook, deleteBook, updateBook } = require('../controllers/bookController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/')
     .get(protect, getBooks)
     .post(protect, createBook);
+
+router.route('/:id')
+    .get(protect, getBook)
+    .delete(protect, deleteBook)
+    .put(protect, updateBook);
 
 module.exports = router;
